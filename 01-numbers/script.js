fe159f24ef498b38d9bc5aeef07b8e6e09ae0f34
NavodContrath <navod.contrath@gmail.com>
2025-02-07 16:10:35 +0100

    add numOnePlusNumTwo

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -8,6 +8,8 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // 1. Imposta questa variabile come somma di numOne e numTwo
 const numOnePlusNumTwo = numOne + numTwo;
 console.log(numOnePlusNumTwo); // Output: 24
+const numOneplusnumTwo = 8 + 16;
+console.log(numOneplusnumTwo); // data option
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
 const numThreeTimesNumTwo = NaN;
